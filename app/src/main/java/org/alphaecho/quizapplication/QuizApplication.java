68816c654209ae0bec264a0c852c320ab24565bf
Arta <aartagashii@hotmail.com>
2016-12-04 23:56:38 +0100

    Krijimi i LogIn perdoruesit, qe permban emrin e perdoruesit, fjalekalimin e perdoruesit dhe butonin  Kyqu i cili ne qastin qe klikohet e dergon perdoruesin ne faqen kryesore te Kuizit qe eshte MainKuizActivity.

--- app/src/main/java/org/alphaecho/quizapplication/QuizApplication.java
+++ app/src/main/java/org/alphaecho/quizapplication/QuizApplication.java
@@ -1,7 +1,11 @@
 package org.alphaecho.quizapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class QuizApplication extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class QuizApplication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_application);
+
+        final Button btnKyqu = (Button) findViewById(R.id.button);
+        EditText edtUser = (EditText) findViewById(R.id.editTxtUser);
+        EditText edtPass = (EditText) findViewById(R.id.editTxtPass);
+
+        btnKyqu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intenti = new Intent(this, MainKuizActivity.class);
+                startActivity(intenti);
+            }
+        });
     }
 }
